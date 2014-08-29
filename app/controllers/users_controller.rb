2f4a63fe48c416752a9dcb90134820e8001411ec
Ned Ruggeri <ruggeri@self-loop.com>
2014-08-28 17:33:52 -0700

    initial auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  before_action :require_not_logged_in!, only: [:create, :new]
+  before_action :require_logged_in!, only: [:show]
+
+  def create
+    # sign up the user
+    @user = User.new(user_params)
+    if @user.save
+      # redirect them to the new user's show page
+      log_in!(@user)
+      redirect_to user_url(@user)
+    else
+      # input didn't pass validation; re-render sign up form.
+      render :new
+    end
+  end
+
+  def new
+    # present form for signup
+    @user = User.new # dummy user object
+    render :new
+  end
+
+  def show
+    if current_user.nil?
+      # let them log in
+      redirect_to new_session_url
+      return
+    end
+
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  protected
+  def user_params
+    self.params.require(:user).permit(:username, :password)
+  end
+end
