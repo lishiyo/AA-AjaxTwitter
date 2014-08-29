bcd96272855c270324b7e8a49897a7ebdeeccc14
Ned Ruggeri <ruggeri@self-loop.com>
2014-08-28 20:43:40 -0700

    fix
    This breaks the helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def current_user
     # fetches the user we've logged in as
     return nil if self.session[:session_token].nil?
-    @user ||= User.find_by(session_token: self.session[:session_token])
+    User.find_by(session_token: self.session[:session_token])
   end
   helper_method :current_user
 
