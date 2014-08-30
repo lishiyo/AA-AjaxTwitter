27a0424aa2e9b4cddb9d51583fad38ca784b0d60
Ned Ruggeri <ruggeri@self-loop.com>
2014-08-30 14:29:26 -0700

    allow interactive search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,10 @@
 //= require jquery_ujs
 //= require_tree .
 
-$.FollowToggle = function (el) {
+$.FollowToggle = function (el, options) {
   this.$el = $(el);
-  this.userId = this.$el.data("user-id");
-  this.followState = this.$el.data("initial-follow-state");
+  this.userId = this.$el.data("user-id") || options.userId;
+  this.followState = this.$el.data("initial-follow-state") || options.followState;
 
   this.render();
 
@@ -74,12 +74,75 @@ $.FollowToggle.prototype.render = function () {
   }
 };
 
-$.fn.followToggle = function () {
+$.fn.followToggle = function (options) {
   return this.each(function () {
-    new $.FollowToggle(this);
+    new $.FollowToggle(this, options);
+  });
+};
+
+$.UsersSearch = function (el) {
+  this.$el = $(el);
+  this.$input = $('<input type="text">');
+  this.$ul = $("<ul></ul>");
+  this.results = [];
+
+  this.$el.append(this.$input);
+  this.$el.append(this.$ul);
+
+  this.$input.on("input", this.handleInput.bind(this));
+};
+
+$.UsersSearch.prototype.handleInput = function (event) {
+  if (this.$input.val() == "") {
+    this.results = [];
+    this.renderResults();
+    return;
+  }
+
+  var usersSearch = this;
+  $.ajax({
+    url: "/users/search",
+    dataType: "json",
+    method: "GET",
+    data: { query: this.$input.val() },
+    success: function (data) {
+      usersSearch.results = data;
+      usersSearch.renderResults();
+    }
+  });
+};
+
+$.UsersSearch.prototype.renderResults = function () {
+  this.$ul.empty();
+
+  for (var i = 0; i < this.results.length; i++) {
+    var user = this.results[i];
+
+    var $a = $("<a></a>");
+    $a.text(user.username);
+    $a.attr("href", "/users/" + user.id);
+
+    var $followToggle = $("<button></button>");
+    $followToggle.followToggle({
+      userId: user.id,
+      followState: user.followed ? "followed" : "unfollowed"
+    });
+
+    var $li = $("<li></li>");
+    $li.append($a);
+    $li.append($followToggle);
+
+    this.$ul.append($li);
+  }
+};
+
+$.fn.usersSearch = function () {
+  return this.each(function () {
+    new $.UsersSearch(this);
   });
 };
 
 $(function () {
   $("button.follow-toggle").followToggle();
+  $("div.users-search").usersSearch();
 });
