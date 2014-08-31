31b582a77a2e80581b7a82f6a9f74dec29139450
Ned Ruggeri <ruggeri@self-loop.com>
2014-08-31 10:41:12 -0700

    fix User#feed_tweets to fetch your own tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,8 @@ class User < ActiveRecord::Base
 
   def feed_tweets(limit = nil, max_created_at = nil)
     @tweets = Tweet
-      .joins(:user => :in_follows)
+      .joins(:user)
+      .joins("LEFT OUTER JOIN follows ON users.id = follows.followee_id")
       .where("tweets.user_id = :id OR follows.follower_id = :id", id: self.id)
       .order("tweets.created_at DESC")
       .uniq
