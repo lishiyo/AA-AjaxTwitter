9e5a0d246b2b8c5aa754e51dffc1fcaf06cb0452
Ned Ruggeri <ruggeri@self-loop.com>
2014-08-30 17:33:20 -0700

    FIX: add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CATS = %w(breakfast earl curie markov gizmo kiki sally)
+
+CATS.each do |cat|
+  u = User.create!(username: cat, password: "#{cat}#{cat}")
+end
+
+MESSAGES = [
+  "Ate some tuna",
+  "Chased a string",
+  "Bounced a ball",
+  "Took a photo of a sandwich",
+  "Took a long nap",
+  "Jumped on some piano keys",
+  "Is that a mouse?",
+  "Scratched a human",
+  "Jumped to the top of the shelf!",
+  "Do cats know how to tweet? YES, we do.",
+  "Set world napping record",
+  "xvyadfoiuasdfnasfdn"
+]
+
+User.all.each do |user|
+  40.times do
+    msg = MESSAGES.sample
+    Tweet.create!(user_id: user.id, content: msg, created_at: rand(10.years).ago)
+  end
+end
